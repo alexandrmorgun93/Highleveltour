7f4cbd9cf1e04e62fc6e6675fc8e4284b78fd6f9
alexandrmorgun93 <alexandrmorgun93@gmail.com>
2017-12-18 00:27:48 +0200

    home

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
 ;
 (() => {
-  'use strict';
-  
-  /*variables*/
+    'use strict';
 
+    /*variables*/
 
+    /*BEGIN PARALLAX*/
+    const Parallax = require('scroll-parallax');
+    let p = new Parallax('.parallax', {
+        intensity:80
+    }).init();
+
+    /*END PARALLAX*/
 
 })();
---
+++ src/js/map.js
@@ -0,0 +1,24 @@
+/*MAP*/
+
+function initMap() {
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: new google.maps.LatLng(50.44, 30.53),
+    });
+
+    let location = [
+        {
+            position: new google.maps.LatLng(50.442610, 30.530780)
+        }
+    ];
+
+    location.forEach(function (locations) {
+        let marker = new google.maps.Marker({
+            position: locations.position,
+            icon: 'img/map-marker.png',
+            map: map
+        });
+    });
+}
+
+/*END MAP*/
