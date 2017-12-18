4d5d5795f0fa12a4609eb0af0a22e3e351eab017
alexandrmorgun93 <alexandrmorgun93@gmail.com>
2017-12-19 00:29:55 +0200

    f

--- src/js/map.js
+++ src/js/map.js
@@ -3,7 +3,7 @@
 function initMap() {
     let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
-        center: new google.maps.LatLng(50.44, 30.53),
+        center: new google.maps.LatLng(50.4422870, 30.5338000),
     });
 
     let location = [
