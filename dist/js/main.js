e3d763d9e4c016c7e40aa6b0e0ce5bf2c361634d
alexandrmorgun93 <alexandrmorgun93@gmail.com>
2017-12-24 01:40:49 +0200

    gg

--- dist/js/main.js
+++ dist/js/main.js
@@ -75,29 +75,33 @@ module.exports = __webpack_require__(4);
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-;
-(() => {
-    'use strict';
+/*BEGIN PARALLAX*/
+const Parallax = __webpack_require__(2);
+let p = new Parallax('.parallax', {
+    intensity: 70
+}).init();
 
-    /*variables*/
+/*END PARALLAX*/
+// begin region menu
+// let regionList = document.querySelector('.btn_region');
+// let region = document.querySelector('.region_content_wrap');
+//
+// regionList.addEventListener('click', function () {
+//     region.classList.toggle('active');
+// });
+// end region menu
 
-    /*BEGIN PARALLAX*/
-    const Parallax = __webpack_require__(2);
-    let p = new Parallax('.parallax', {
-        intensity: 70
-    }).init();
 
-    /*END PARALLAX*/
-    // begin region menu
-    let regionList = document.querySelector('.btn_region');
-    let region = document.querySelector('.region_content_wrap');
-
-    regionList.addEventListener('click', function () {
-        region.classList.toggle('active');
+// BEGIN TABS
+let tabsBtn = document.querySelectorAll('.tabs_btn');
+for (let i = 0; i < tabsBtn.length; i++) {
+    tabsBtn[i].addEventListener('click', function () {
+        this.classList.toggle('active');
     });
-    // end region menu
+}
+console.log('tabsBtn');
+// END TABS
 
-})();
 
 
 /***/ }),
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,27 @@
-;
-(() => {
-    'use strict';
+/*BEGIN PARALLAX*/
+const Parallax = require('scroll-parallax');
+let p = new Parallax('.parallax', {
+    intensity: 70
+}).init();
 
-    /*variables*/
+/*END PARALLAX*/
+// begin region menu
+// let regionList = document.querySelector('.btn_region');
+// let region = document.querySelector('.region_content_wrap');
+//
+// regionList.addEventListener('click', function () {
+//     region.classList.toggle('active');
+// });
+// end region menu
 
-    /*BEGIN PARALLAX*/
-    const Parallax = require('scroll-parallax');
-    let p = new Parallax('.parallax', {
-        intensity: 70
-    }).init();
 
-    /*END PARALLAX*/
-    // begin region menu
-    let regionList = document.querySelector('.btn_region');
-    let region = document.querySelector('.region_content_wrap');
-
-    regionList.addEventListener('click', function () {
-        region.classList.toggle('active');
+// BEGIN TABS
+let tabsBtn = document.querySelectorAll('.tabs_btn');
+for (let i = 0; i < tabsBtn.length; i++) {
+    tabsBtn[i].addEventListener('click', function () {
+        this.classList.toggle('active');
     });
-    // end region menu
+}
+console.log('tabsBtn');
+// END TABS
 
-})();
