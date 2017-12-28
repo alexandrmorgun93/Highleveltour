d00820b947e5f003c56f84f13f0329441402e75b
alexandrmorgun93 <alexandrmorgun93@gmail.com>
2017-12-28 22:21:35 +0200

    g

--- dist/js/main.js
+++ dist/js/main.js
@@ -83,12 +83,12 @@ let p = new Parallax('.parallax', {
 
 /*END PARALLAX*/
 // begin region menu
-// let regionList = document.querySelector('.btn_region');
-// let region = document.querySelector('.region_content_wrap');
-//
-// regionList.addEventListener('click', function () {
-//     region.classList.toggle('active');
-// });
+let regionList = document.querySelector('.btn_region');
+let region = document.querySelector('.region_content_wrap');
+
+regionList.addEventListener('click', function () {
+    region.classList.toggle('active');
+});
 // end region menu
 
 
